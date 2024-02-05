bdd86d8b792f6c07cd9ea332a8343631c6b8c3ed
Olcay <1018693@hr.nl>
2024-02-05 16:54:01 +0100

    - CSS toegevoegd - icon toegevoegd - html bijgewerkt

--- ervaringsdeskundige/views.py
+++ ervaringsdeskundige/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard_deskundige.html')
