fb035e670ca635573f6f38880672bb34944eea84
Omer <1080314@hr.nl>
2024-02-05 15:46:50 +0100

    - Template naam gewijzigd van dashboard.html naar dashboard_beheer.html - Routing aangepast

--- beheerders/views.py
+++ beheerders/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard_beheer.html')
