be2f07e23063da84abf1399ba0978095bfcec70a
Adam-1052189 <1052189@hr.nl>
2024-02-14 19:17:13 +0100

    Javascript werkt in appart bestand, Inlogpagina HTML aangepast

--- log_in/static/JS/log_in.js
+++ log_in/static/JS/log_in.js
@@ -1,5 +1,4 @@
 function wisInloggegevens(){
     document.getElementById("mailadres").value = "";
     document.getElementById("wachtwoord").value = "";
-    console.log("functie wordt aangeroepen")
 }
