7ef5adaf466e3e26b6b5f3820a5539e5b9f7d2a7
Olcay <1018693@hr.nl>
2024-02-18 17:29:58 +0100

    - de url aangepast van de registreer ervraingsdeskundige. -knop toegevoegd bij login pagina die verwijst naar de registratie pagina

--- ervaringsdeskundige/urls.py
+++ ervaringsdeskundige/urls.py
@@ -3,6 +3,5 @@ from .views import dashboard, aanmelden_voor_onderzoek, registratie_ervaringsdes
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
-    path('registratie_ervaringsdeskundige/',registratie_ervaringsdeskundige),
     path('aanmelden/<int:onderzoek_id>/', aanmelden_voor_onderzoek, name='aanmelden_voor_onderzoek'),
 ]
--- log_in/urls.py
+++ log_in/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from ervaringsdeskundige.views import registratie_ervaringsdeskundige
 
 urlpatterns = [
     path('', views.login, name="login"),
+    path('registratie/', registratie_ervaringsdeskundige, name="registratie"),
 ]
