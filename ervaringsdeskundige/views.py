1ba0b41bf0fbb76884f5e856f33bbcbc62a80e9e
Olcay <1018693@hr.nl>
2024-02-20 14:29:45 +0100

    - wijziging aan de registratie functie

--- ervaringsdeskundige/views.py
+++ ervaringsdeskundige/views.py
@@ -4,26 +4,22 @@ from core.models import Gebruikers, Beperkingen, Toezichthouder, Hulpmiddelen, E
 from django.views import View
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponse
-from core.decorators import custom_login_required
 from django.contrib.auth.hashers import make_password  # Voor wachtwoord hashing
 
 def registratie_ervaringsdeskundige(request):
     if request.method == 'POST':
         form = RegistratieFormulier(request.POST)
         if form.is_valid():
-            # Hier voeren we de wachtwoord hashing uit
-            gehasht_wachtwoord = make_password(form.cleaned_data['wachtwoord'])
-
-            # Gebruiker object aanmaken en opslaan met gehasht wachtwoord
-            gebruiker = Gebruikers(
+            gebruiker = Gebruikers.objects.create_user(
                 voornaam=form.cleaned_data['voornaam'],
                 achternaam=form.cleaned_data['achternaam'],
                 postcode=form.cleaned_data['postcode'],
                 geslacht=form.cleaned_data['geslacht'],
                 email=form.cleaned_data['email'],
-                wachtwoord=gehasht_wachtwoord,
+                password=form.cleaned_data['wachtwoord'],
                 telefoonnummer=form.cleaned_data['telefoonnummer'],
                 geboortedatum=form.cleaned_data['geboortedatum']
             )
@@ -75,7 +71,7 @@ def registratie_ervaringsdeskundige(request):
 
     return render(request, 'registratie_ervaringsdeskundige.html', {'form': form})
 
-@custom_login_required
+
 def dashboard(request):
     gebruiker_id = request.user.id  # Veronderstelt dat je een manier hebt om de huidige gebruiker te krijgen
     ervaringsdeskundige = Ervaringsdeskundige.objects.filter(gebruiker_id=gebruiker_id).first()
@@ -96,7 +92,7 @@ def aanmelden_voor_onderzoek(request, onderzoek_id):
             onderzoek = Onderzoek.objects.get(id=onderzoek_id)
             OnderzoekErvaringsdeskundige.objects.create(onderzoek=onderzoek, ervaringsdeskundige=ervaringsdeskundige)
             messages.success(request, 'Je bent succesvol aangemeld voor het onderzoek.')
-            return redirect('dashboard')
+            return redirect('dashboard_deskundige')
         else:
             return HttpResponse('Je moet een ervaringsdeskundige zijn om je aan te melden voor een onderzoek.', status=403)
 
