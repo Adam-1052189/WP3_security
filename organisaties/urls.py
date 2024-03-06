d7c0e94db06e4d2c7f9a3ceb5bb676c43ad73f52
Adam-1052189 <1052189@hr.nl>
2024-03-06 11:43:42 +0100

    - uitlogknop toegevoegd - link naar maak organisatie aan knop

--- organisaties/urls.py
+++ organisaties/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('onderzoekstabel/', onderzoekstabel_view, name='dashboard_organisatie'),
     path('onderzoek/invoeren/', views.onderzoek_invoeren, name='onderzoek_invoeren'),
     path('maak_organisatie_aan/', views.maak_organisatie_aan, name='maak_organisatie_aan'),
+    path('logout/', views.logout_view, name='logout'),
 ]
