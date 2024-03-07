220a7d8bfefb802f9de0e16763c2bb4c8be02a8b
Olcay <1018693@hr.nl>
2024-03-07 13:30:50 +0100

    - Bijgewerkt zodat alleen maar de onderzoeken met status 'Goedgekeurd' te zien zijn en waarvoor je nog niet heb deelgenomen.

--- ervaringsdeskundige/views.py
+++ ervaringsdeskundige/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import RegistratieFormulier, GebruikerForm, CustomPasswordChangeForm, ErvaringsdeskundigeForm, BeperkingenForm, HulpmiddelenForm, ToezichthouderForm
-from core.models import Gebruikers, Beperkingen, Toezichthouder, Hulpmiddelen, Ervaringsdeskundige, Onderzoek, OnderzoekErvaringsdeskundige
+from .forms import (RegistratieFormulier, GebruikerForm, CustomPasswordChangeForm, ErvaringsdeskundigeForm,
+                    BeperkingenForm, HulpmiddelenForm, ToezichthouderForm)
+from core.models import (Gebruikers, Beperkingen, Toezichthouder, Hulpmiddelen, Ervaringsdeskundige, Onderzoek,
+                         OnderzoekErvaringsdeskundige)
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import get_user_model, update_session_auth_hash, logout
 from datetime import date
-from django.views.decorators.http import require_POST
+
 
 def registratie_ervaringsdeskundige(request):
     if request.method == 'POST':
@@ -69,13 +71,17 @@ def registratie_ervaringsdeskundige(request):
 
     return render(request, 'registratie_ervaringsdeskundige.html', {'form': form})
 
+
 User = get_user_model()
+
 
 @login_required()
 def dashboard(request):
     ervaringsdeskundige = Ervaringsdeskundige.objects.filter(gebruiker=request.user).first()
     if ervaringsdeskundige:
-        beschikbare_onderzoeken = Onderzoek.objects.filter(beschikbaar=True)
+        beschikbare_onderzoeken = Onderzoek.objects.exclude(
+            onderzoekervaringsdeskundige__ervaringsdeskundige=ervaringsdeskundige).filter(status='Goedgekeurd',
+                                                                                          beschikbaar=True)
         context = {
             'beschikbare_onderzoeken': beschikbare_onderzoeken,
         }
@@ -83,13 +89,14 @@ def dashboard(request):
     else:
         return HttpResponse('Je moet een ervaringsdeskundige zijn om deze pagina te bekijken.', status=403)
 
-@require_POST
+
 @login_required
 def aanmelden_voor_onderzoek(request, onderzoek_id):
     onderzoek = get_object_or_404(Onderzoek, pk=onderzoek_id)
     ervaringsdeskundige = get_object_or_404(Ervaringsdeskundige, gebruiker=request.user)
 
-    koppeling_bestaat = OnderzoekErvaringsdeskundige.objects.filter(onderzoek=onderzoek, ervaringsdeskundige=ervaringsdeskundige).exists()
+    koppeling_bestaat = OnderzoekErvaringsdeskundige.objects.filter(onderzoek=onderzoek,
+                                                                    ervaringsdeskundige=ervaringsdeskundige).exists()
 
     if not koppeling_bestaat:
         OnderzoekErvaringsdeskundige.objects.create(
@@ -100,6 +107,7 @@ def aanmelden_voor_onderzoek(request, onderzoek_id):
         return JsonResponse({"success": True, "message": "Je bent succesvol aangemeld voor het onderzoek."})
     else:
         return JsonResponse({"success": False, "message": "Je bent al aangemeld voor dit onderzoek."})
+
 
 @login_required()
 def onderzoek_details(request, onderzoek_id):
@@ -121,10 +129,13 @@ def onderzoek_details(request, onderzoek_id):
     }
     return render(request, 'onderzoek_details.html', context)
 
+
 def leeftijd_berekenen(geboortedatum):
     vandaag = date.today()
-    leeftijd = vandaag.year - geboortedatum.year - ((vandaag.month, vandaag.day) < (geboortedatum.month, geboortedatum.day))
+    leeftijd = vandaag.year - geboortedatum.year - ((vandaag.month, vandaag.day)
+                                                    < (geboortedatum.month, geboortedatum.day))
     return leeftijd
+
 
 @login_required
 def profiel(request):
@@ -134,14 +145,16 @@ def profiel(request):
     ervaringsdeskundige, _ = Ervaringsdeskundige.objects.get_or_create(gebruiker=gebruiker)
     hulpmiddelen, _ = Hulpmiddelen.objects.get_or_create(ervaringsdeskundige=ervaringsdeskundige)
     beperkingen, _ = Beperkingen.objects.get_or_create(ervaringsdeskundige=ervaringsdeskundige)
-    toezichthouder, _ = Toezichthouder.objects.get_or_create(ervaringsdeskundige=ervaringsdeskundige) if toon_toezichthouder_form else (None, False)
+    toezichthouder, _ = Toezichthouder.objects.get_or_create(ervaringsdeskundige=ervaringsdeskundige) if (
+        toon_toezichthouder_form) else (None, False)
 
     if request.method == 'POST':
         gebruiker_form = GebruikerForm(request.POST, instance=gebruiker)
         ervaringsdeskundige_form = ErvaringsdeskundigeForm(request.POST, instance=ervaringsdeskundige)
         hulpmiddelen_form = HulpmiddelenForm(request.POST, instance=hulpmiddelen)
         beperkingen_form = BeperkingenForm(request.POST, instance=beperkingen)
-        toezichthouder_form = ToezichthouderForm(request.POST, instance=toezichthouder) if toon_toezichthouder_form else None
+        toezichthouder_form = ToezichthouderForm(request.POST, instance=toezichthouder) if (
+            toon_toezichthouder_form) else None
         password_form = CustomPasswordChangeForm(user=gebruiker, data=request.POST)
 
         if 'wijzigen_profiel' in request.POST:
@@ -183,6 +196,7 @@ def profiel(request):
         'toon_toezichthouder_form': toon_toezichthouder_form,
     }
     return render(request, 'profiel.html', context)
+
 
 def logout_view(request):
     if request.method == 'POST':
